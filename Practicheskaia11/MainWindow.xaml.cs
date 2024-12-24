de36f7f2913e984e7bbff3a8d92cfbb6e2cf53bf
Артём Жаров <artemzharov123456@outlook.com>
2024-12-24 19:36:38 +0300

    Добавьте файлы проекта.

---
+++ Practicheskaia11/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Practicheskaia11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string s = "aa afa aba abba abbba abca abea";
+        private void btRegex(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"aa|aba|abba|abbba");
+            Match match = regex.Match(s);
+            if (match.Value != "")
+            {
+                MatchCollection matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    object[] mas = new object[matches.Count];
+                    matches.CopyTo(mas, 0);
+                    listBox.ItemsSource = mas;
+                }
+            }
+        }
+
+        private void btFound(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"(a[a-f]a)");
+            Match match = regex.Match(s);
+            if (match.Value != "")
+            {
+                MatchCollection matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    object[] mas = new object[matches.Count];
+                    matches.CopyTo(mas, 0);
+                    listBox.ItemsSource = mas;
+                }
+            }
+        }
+
+        private void btInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Сюда туда");
+        }
+
+        private void btExit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
